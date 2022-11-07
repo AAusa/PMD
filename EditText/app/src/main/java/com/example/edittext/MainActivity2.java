dcbf6f83b0c159cdcffc48090c7fe7b94ec0e07c
alu <alu@PC64>
2022-11-07 14:16:31 +0100

    colores

--- EditText/app/src/main/java/com/example/edittext/MainActivity2.java
+++ EditText/app/src/main/java/com/example/edittext/MainActivity2.java
@@ -3,7 +3,9 @@ package com.example.edittext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,10 +14,41 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         RadioGroup miGrupo = (RadioGroup) findViewById(R.id.grupoRadio);
+        TextView tv = (TextView) findViewById(R.id.textView);
+
         miGrupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                //cod
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                RadioButton rb = (RadioButton) findViewById(checkedId);
+                String nuevoTexto = "Pulsado: " + rb.getText().toString();
+                tv.setText(nuevoTexto);
+                /*
+                String texto = null;
+                switch(checkedId) {
+                    case 2131231203:
+                        texto = "Lunes";
+                        break;
+                    case 2131231204:
+                        texto = "Martes";
+                        break;
+                    case 2131231205:
+                        texto = "Miercoles";
+                        break;
+                    case 2131231202:
+                        texto = "Jueves";
+                        break;
+                    case 2131231208:
+                        texto = "Viernes";
+                        break;
+                    case 2131231206:
+                        texto = "Sabado";
+                        break;
+                    case 2131231200:
+                        texto = "Domingo";
+                        break;
+                }
+                tv.setText("PULSADO: " + texto);
+                */
             }
         });
     }
